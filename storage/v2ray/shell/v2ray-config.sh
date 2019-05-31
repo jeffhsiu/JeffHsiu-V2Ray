1cb6c1d8204359de5c5e36e5b0ff454ba86b8ae2
Jeff Lin <jeff.lin@zeepin.com>
2019-05-31 12:24:49 +0800

    fix shell

--- storage/v2ray/shell/v2ray-config.sh
+++ storage/v2ray/shell/v2ray-config.sh
@@ -99,13 +99,27 @@ create_vmess_URL_config()
 EOF
 }
 
+base64_vmess()
+{
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        # Linux
+        vmess="vmess://$(cat /usr/local/etc/v2ray/vmess_qr.json | base64 -w 0)"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        vmess="vmess://$(cat /usr/local/etc/v2ray/vmess_qr.json | base64 -b 0)"
+    else
+        # Unknown.
+        vmess="vmess://$(cat /usr/local/etc/v2ray/vmess_qr.json | base64 -w 0)"
+    fi
+}
+
 export_config()
 {
 	if [ ! -d ${path} ]; then
 		mkdir -p ${path}
 	fi
-	local vmess="vmess://$(cat /usr/local/etc/v2ray/vmess_qr.json | base64 -b 0)"
-	cat << EOF > ${path}/config-${index}.txt
+
+    cat << EOF > ${path}/config-${index}.txt
 ------------- V2Ray 配置信息 -------------
  地址 (Address) = ${ip}
  端口 (Port) = ${port}
@@ -126,16 +140,7 @@ export_qrcode()
 	if [ ! -d ${path} ]; then
 		mkdir -p ${path}
 	fi
-	if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        # Linux
-        local vmess="vmess://$(cat /usr/local/etc/v2ray/vmess_qr.json | base64 -w 0)"
-	elif [[ "$OSTYPE" == "darwin"* ]]; then
-	    # Mac OSX
-        local vmess="vmess://$(cat /usr/local/etc/v2ray/vmess_qr.json | base64 -b 0)"
-    else
-        # Unknown.
-        local vmess="vmess://$(cat /usr/local/etc/v2ray/vmess_qr.json | base64 -w 0)"
-    fi
+
 	link="http://api.jeffhsiu.com/qrcode/create?size=460&error=Q&text=${vmess}"
 	wget -O ${path}/qrcode-${index}.png ${link}
 }
@@ -148,6 +153,7 @@ do
     port=`expr 5550 + ${index}`
     create_v2ray_config
     create_vmess_URL_config
+    base64_vmess
     export_config
     export_qrcode
 done
