dbf1251520b7aadabd6c6e191f917b40ecc8f1ee
Jeff Lin <jeff.lin@zeepin.com>
2019-06-27 10:33:51 +0800

    add order expired check limit

--- app/Console/Commands/CheckOrderEndDate.php
+++ app/Console/Commands/CheckOrderEndDate.php
@@ -52,67 +52,75 @@ class CheckOrderEndDate extends Command
                 Log::error('Server dose not exists. order_id:' . $order->id . ', server_id:'.$order->server_id);
                 continue;
             }
-            $username = 'root';
-            $password = $server->ssh_pwd;
-            $ip = $server->ip;
-            $port = $server->ssh_port;
-            $docker_name = $order->docker_name;
-            $docker_name_unit = substr($order->docker_name, 0, -2).(substr($order->docker_name, -2)+0);
 
-            try {
-                $connection = ssh2_connect($ip, $port);
-                ssh2_auth_password($connection, $username, $password);
+            if ( !Order::where('server_id', $order->server_id)
+                ->where('docker_name', $order->docker_name)
+                ->where('status', Order::STATUS_ENABLE)
+                ->where('end_date', '>', Carbon::today())
+                ->exists()) {
 
-                ssh2_exec($connection, 'docker stop '.$docker_name.' '.$docker_name_unit);
+                $username = 'root';
+                $password = $server->ssh_pwd;
+                $ip = $server->ip;
+                $port = $server->ssh_port;
+                $docker_name = $order->docker_name;
+                $docker_name_unit = substr($order->docker_name, 0, -2).(substr($order->docker_name, -2)+0);
 
-                ssh2_exec($connection, 'exit');
-                unset($connection);
+                try {
+                    $connection = ssh2_connect($ip, $port);
+                    ssh2_auth_password($connection, $username, $password);
 
-            } catch (\Exception $exception) {
-                Log::error('Docker stop failed. error: '.$exception->getMessage());
-                continue;
+                    ssh2_exec($connection, 'docker stop '.$docker_name.' '.$docker_name_unit);
+
+                    ssh2_exec($connection, 'exit');
+                    unset($connection);
+
+                } catch (\Exception $exception) {
+                    Log::error('Docker stop failed. error: '.$exception->getMessage());
+                    continue;
+                }
+
+                // 伺服器操作記錄
+                ServerLog::create([
+                    'user_id' => ServerLog::USER_ID_SYSTEM,
+                    'server_id' => $server->id,
+                    'order_id' => $order->id,
+                    'ip' => $server->ip,
+                    'docker_name' => $order->docker_name,
+                    'action' => ServerLog::ACTION_DOCKER_STOP,
+                    'reason' => 'Order end date expired.'
+                ]);
+
+                // Wechat消息推送
+                foreach (config('wechatpush.send_key') as $send_key) {
+                    $text = $order->customer->name.'_使用日到期，Docker已暫停';
+                    $desp = sprintf('Customer: %s'.PHP_EOL.PHP_EOL.
+                        'IP: %s'.PHP_EOL.PHP_EOL.
+                        'Docker name: %s'.PHP_EOL.PHP_EOL.
+                        'Action: %s'.PHP_EOL.PHP_EOL.
+                        'Reason: %s',
+                        $order->customer->name,
+                        $order->server->ip,
+                        $order->docker_name,
+                        'Docker 停止',
+                        '訂單使用日到期'
+                    );
+                    try {
+                        $url = sprintf(
+                            'https://sc.ftqq.com/%s.send?text=%s&desp=%s',
+                            urlencode($send_key),
+                            urlencode($text),
+                            urlencode($desp)
+                        );
+                        file_get_contents($url);
+                    } catch (\Exception $e) {
+                        Log::error('Wechat push error:'.$e->getMessage());
+                    }
+                }
             }
 
             // 訂單設為過期狀態
             $order->update(['status' => Order::STATUS_EXPIRED]);
-
-            // 伺服器操作記錄
-            ServerLog::create([
-                'user_id' => ServerLog::USER_ID_SYSTEM,
-                'server_id' => $server->id,
-                'order_id' => $order->id,
-                'ip' => $server->ip,
-                'docker_name' => $order->docker_name,
-                'action' => ServerLog::ACTION_DOCKER_STOP,
-                'reason' => 'Order end date expired.'
-            ]);
-
-            // Wechat消息推送
-            foreach (config('wechatpush.send_key') as $send_key) {
-                $text = $order->customer->name.'_使用日到期，Docker已暫停';
-                $desp = sprintf('Customer: %s'.PHP_EOL.PHP_EOL.
-                    'IP: %s'.PHP_EOL.PHP_EOL.
-                    'Docker name: %s'.PHP_EOL.PHP_EOL.
-                    'Action: %s'.PHP_EOL.PHP_EOL.
-                    'Reason: %s',
-                    $order->customer->name,
-                    $order->server->ip,
-                    $order->docker_name,
-                    'Docker 停止',
-                    '訂單使用日到期'
-                );
-                try {
-                    $url = sprintf(
-                        'https://sc.ftqq.com/%s.send?text=%s&desp=%s',
-                        urlencode($send_key),
-                        urlencode($text),
-                        urlencode($desp)
-                    );
-                    file_get_contents($url);
-                } catch (\Exception $e) {
-                    Log::error('Wechat push error:'.$e->getMessage());
-                }
-            }
         }
     }
 }
